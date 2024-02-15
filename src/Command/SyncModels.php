d2d44eb993dde64745215bb5d6e10813cd04e3ca
Michel Descoteaux <descoteaux.michel@gmail.com>
2024-02-15 15:09:46 -0500

    Sync models - display list of changes
    add --dry option

--- src/Command/SyncModels.php
+++ src/Command/SyncModels.php
@@ -6,11 +6,12 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Charcoal\Conductor\Traits\ModelAwareTrait;
 use Charcoal\Conductor\Traits\TimerTrait;
-use Charcoal\Model\Model;
 use Charcoal\Model\ModelInterface;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Input\InputArgument;
+use Charcoal\Source\DatabaseSource;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Helper\Table;
 
 class SyncModels extends AbstractCommand
 {
@@ -18,6 +19,7 @@ class SyncModels extends AbstractCommand
     use ModelAwareTrait;
 
     private $model_classes;
+    private $isDryRun = false;
 
     /**
      * @return void
@@ -28,6 +30,7 @@ class SyncModels extends AbstractCommand
             ->setName('models:db')
             ->setDescription('Synchronize the database with model definitions.')
             ->addArgument('operation', InputArgument::OPTIONAL, 'update or create')
+            ->addOption('dry', null, InputOption::VALUE_NONE, 'Dry-run')
             ->setHelp(<<<'EOF'
 The <info>%command.name%</info> command displays a list of all registered models/objects within your Charcoal project
 EOF
@@ -49,11 +52,19 @@ EOF
         }
 
         $models = $this->loadModels($model_factory, $output);
+        $models = array_filter($models, function ($model) {
+            return !empty($model->metadata()->get('sources'));
+        });
         $modelsCount = count($models);
 
-        if (empty($modelsCount)) {
+        if (empty($models)) {
             $output->writeln('No models were found in the current directory.');
             return 0;
+        }
+
+        $this->isDryRun = $input->getOption('dry');
+        if ($this->isDryRun) {
+            $output->writeln('<fg=red;options=bold>DRY RUN</>');
         }
 
         switch ($input->getArgument('operation')) {
@@ -101,7 +112,7 @@ EOF
     private function createTable(ModelInterface $model, OutputInterface $output)
     {
         $class_name = get_class($model);
-        $output->write(sprintf('<fg=green;>Creating</> table for <fg=yellow;options=bold>%s</>', $class_name));
+        $output->write(sprintf('<fg=green>Creating table</> for <fg=yellow;options=bold>%s</>', $class_name));
         $model->source()->createTable();
         $output->writeln(sprintf(' - %ss', $this->timer()->stop()));
     }
@@ -109,8 +120,107 @@ EOF
     private function updateTable(ModelInterface $model, OutputInterface $output)
     {
         $class_name = get_class($model);
-        $output->write(sprintf('Updating table for <fg=yellow;options=bold>%s</>', $class_name));
-        $model->source()->alterTable();
-        $output->writeln(sprintf(' - %ss', $this->timer()->stop()));
+        $updateMessage = sprintf('<fg=blue>Updating table</> for <fg=yellow;options=bold>%s</>', $class_name);
+        $changes = $this->getChanges($model, $output);
+        if ($changes instanceof Table) {
+            $output->writeln($updateMessage);
+            $changes->render();
+
+            if (!$this->isDryRun) {
+                $model->source()->alterTable();
+                $output->writeln(sprintf(
+                    '<fg=green>Updated %s - %ss</>',
+                    $model->source()->table(),
+                    $this->timer()->stop()
+                ));
+            }
+        } else {
+            $output->writeln(sprintf('Skipping <fg=yellow;options=bold>%s</>: already up-to-date', $class_name));
+        }
+    }
+
+    private function getChanges(ModelInterface $model, OutputInterface $output): ?Table
+    {
+        /** @var DatabaseSource $source */
+        $source = $model->source();
+        $fields = $this->getModelFields($model);
+        $tableStructure = $source->tableStructure();
+
+        $table = (new Table($output))->setHeaders(['Property', 'Aspect', 'Old', 'New']);
+        $alterations = 0;
+
+        foreach ($fields as $field) {
+            $ident = $field->ident();
+
+            if (!array_key_exists($ident, $tableStructure)) {
+                $fieldSql = $field->sql();
+                if ($fieldSql) {
+                    $table->addRow([$ident, '--', '--', 'CREATE']);
+                }
+            } else {
+                // The key exists. Validate.
+                $col   = $tableStructure[$ident];
+
+                if (strtolower($col['Type']) !== strtolower($field->sqlType())) {
+                    // types do not match.
+                    $sqlType = strtolower($field->sqlType());
+                    $sqlType = preg_replace('/(int)(\(\d+\))/', '$1', $sqlType, 1);
+
+                    if ($sqlType !== strtolower($col['Type'])) {
+                        $alterations++;
+                        $table->addRow([
+                            $ident,
+                            'type',
+                            strtolower($col['Type']),
+                            strtolower($field->sqlType())
+                        ]);
+                    }
+                }
+
+                if ((strtolower($col['Null']) !== 'no') !== $field->allowNull() && empty($col['Key'])) {
+                    // Allow null.
+                    $alterations++;
+                    $table->addRow([
+                        $ident,
+                        'allow_null',
+                        (strtolower($col['Null']) !== 'no') ? 'true' : 'false',
+                        $field->allowNull() ? 'true' : 'false'
+                    ]);
+                }
+
+                if ($col['Default'] !== $field->defaultVal()) {
+                    // Change default value.
+                    $alterations++;
+                    $table->addRow([
+                        $ident,
+                        'default_val',
+                        $col['Default'],
+                        $field->defaultVal()
+                    ]);
+                }
+            }
+        }
+
+        return $alterations ? $table : null;
+    }
+
+    private function getModelFields(ModelInterface $model)
+    {
+        $properties = array_keys($model->metadata()->properties());
+
+        $fields = [];
+        foreach ($properties as $propertyIdent) {
+            $prop = $model->property($propertyIdent);
+            if (!$prop || !$prop['active'] || !$prop['storable']) {
+                continue;
+            }
+
+            $val = $model->propertyValue($propertyIdent);
+            foreach ($prop->fields($val) as $fieldIdent => $field) {
+                $fields[$field->ident()] = $field;
+            }
+        }
+
+        return $fields;
     }
 }
