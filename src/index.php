78bc45f41e3aafedfdc18fd7fb74108753244029
Michel Descoteaux <descoteaux.michel@gmail.com>
2024-02-16 11:51:09 -0500

    Fix missing autoloader
    Fix global autoloader

--- src/index.php
+++ src/index.php
@@ -4,6 +4,15 @@ namespace Charcoal\Conductor;
 
 use Charcoal\Conductor\Model\Conductor;
 
-require_once(__DIR__ . '/../vendor/autoload.php');
+$autoload_paths = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php'
+];
+foreach ($autoload_paths as $autoload_path) {
+    if (file_exists($autoload_path)) {
+        require_once($autoload_path);
+        break;
+    }
+}
 
-$conductor = new Conductor();
+new Conductor();
